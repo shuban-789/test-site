6aca67f95aec2e50fb3a04c7eb3335ebf685f9d4
shuban-789 <cybergeek11929@gmail.com>
2025-02-20 15:54:39 -0800

    e

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -6,9 +6,9 @@ export interface Framework {
 }
 
 export const frameworks: Framework[] = [
-    { name: 'astro', displayName: 'Astro', path: '/', color: '#FF5D01' },
-    { name: 'react', displayName: 'React', path: '/react', color: '#61DAFB' },
-    { name: 'vuejs', displayName: 'Vue', path: '/vue', color: '#4FC08D' },
-    { name: 'svelte', displayName: 'Svelte', path: '/svelte', color: '#FF3E00' },
-    { name: 'solidjs', displayName: 'Solid', path: '/solid', color: '#2C4F7C' },
+    { name: 'home', displayName: 'Home', path: '/', color: '#FF5D01' },
+    { name: 'about', displayName: 'About', path: '/about', color: '#61DAFB' },
+    { name: 'gallery', displayName: 'Team', path: '/team', color: '#4FC08D' },
+    { name: 'team', displayName: 'Gallery', path: '/gallery', color: '#FF3E00' },
+    { name: 'stats', displayName: 'Stats', path: 'https://ftcscout.org/teams/12499', color: '#2C4F7C' },
 ];
