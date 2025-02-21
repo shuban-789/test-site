57b17974304e979855c63894614c2be2bda768ea
shuban-789 <cybergeek11929@gmail.com>
2025-02-20 17:23:58 -0800

    e

--- src/sponsors.ts
+++ src/sponsors.ts
@@ -1,17 +1,19 @@
+
+
 export const sponsors = [
     {
         name: "Leidos",
-        logo: "./sponsors/image19.png",
+        logo: "/sponsors/image19.png",
         url: "https://www.leidos.com/",
     },
     {
         name: "Rise",
-        logo: "./sponsors/image23.png",
+        logo: "/sponsors/image23.png",
         url: "https://www.rise4steam.org/",
     },
     {
         name: "Qualcomm",
-        logo: "./sponsors/image39.png",
+        logo: "/sponsors/image39.png",
         url: "https://www.qualcomm.com/",
     }
 ];
