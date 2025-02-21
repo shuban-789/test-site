f2011dd13db33f60f7eee4036e273f9e7b12c72a
shuban-789 <cybergeek11929@gmail.com>
2025-02-20 16:18:25 -0800

    e

---
+++ src/sponsors.ts
@@ -0,0 +1,17 @@
+export const sponsors = [
+    {
+        name: "Leidos",
+        logo: "/sponsors/image19.png",
+        url: "https://company-a.com",
+    },
+    {
+        name: "Rise",
+        logo: "/sponsors/image23.png",
+        url: "https://company-b.com",
+    },
+    {
+        name: "Qualcomm",
+        logo: "/sponsors/image39.png",
+        url: "https://company-c.com",
+    }
+];
