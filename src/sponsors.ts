bb14d8c8fa8888308fbee3308c12b01efac41b28
shuban-789 <cybergeek11929@gmail.com>
2025-02-20 19:38:33 -0800

    e

--- src/sponsors.ts
+++ src/sponsors.ts
@@ -13,5 +13,20 @@ export const sponsors = [
         name: "ASML",
         logo: "sponsors/asml.png",
         url: "https://www.asml.com/en",
-    }
+    },
+    {
+        name: "HP",
+        logo: "sponsors/hp.png",
+        url: "https://www.hp.com/us-en/home.html",
+    },
+    {
+        name: "ICE",
+        logo: "sponsors/ice.png",
+        url: "https://www.ice.com/index",
+    },
+    {
+        name: "goBILDA",
+        logo: "sponsors/gobilda.png",
+        url: "https://www.gobilda.com/",
+    },
 ];
